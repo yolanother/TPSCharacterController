6bd38af1c7cd7143d7e08249212474356f833645
Aaron Jackson <yolanother@gmail.com>
2021-03-30 23:26:25 -0700

    Add odin stubs to make odin usage cleaner

---
+++ Scripts/Runtime/Odin/OdinStubs.cs
@@ -0,0 +1,44 @@
+﻿#if !ODIN_INSPECTOR
+// This is a collection of stubs or quick hacks to match the
+// odin apis that we are using.
+
+#if !ODIN_STUBS
+#define ODIN_STUBS
+
+using System;
+using System.Collections.Generic;
+
+namespace Sirenix.OdinInspector
+{
+    public class Button : Attribute
+    {
+        
+    }
+
+    public class InlineEditor : Attribute
+    {
+        
+    }
+
+    public class HideIf : Attribute
+    {
+        public HideIf(string condition) {}
+    }
+}
+
+namespace Sirenix.Utilities
+{
+    public static class HashSetUtilities
+    {
+        public static void AddRange<T>(this HashSet<T> set, IEnumerable<T> values)
+        {
+            foreach (var value in values)
+            {
+                set.Add(value);
+            }
+        }
+    }
+}
+
+#endif
+#endif
